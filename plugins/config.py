6d3c015f5ae2057eba677d258b49de40dbaf4c5f
Sandalivihansa <114420339+Sandalivihansa@users.noreply.github.com>
2024-12-29 00:37:58 -0800

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7332398186:AAEXqQxzOTqyWY2NAfzRQ5l3HbVT3t5ace4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7834660675:AAFFnOq4zat4HZhTfXGwg2h5NAO3o-QLp0g")
     
     API_ID = int(os.environ.get("API_ID", "5047271"))
     
